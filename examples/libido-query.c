8746a7596d17c80f03a1c7d1ca84645405009bc0
xsbee <xyesbee@protonmail.com>
2022-12-10 11:20:33 +0530

    Git merges are a mess

--- examples/libido-query.c
+++ examples/libido-query.c
@@ -8,119 +8,13 @@
 #include "args.h"
 #include "util.h"
 
-static struct
-{
-  enum libido_search_order order;
-  const char *repr;
-}
-const order_strs[5] =
-{
-  {LIBIDO_ORDER_BY_UPLOAD_DATE, "upload"},
-  {LIBIDO_ORDER_BY_VIEWS, "views"},
-  {LIBIDO_ORDER_BY_LIKES, "likes"},
-  {LIBIDO_ORDER_BY_RELEASE_DATE, "release"},
-  {LIBIDO_ORDER_BY_TITLE, "title"}
-};
-
-static
-void print_usage (const char *name)
-{
-  fprintf (stderr, "%s [-aA] [-b brands ...] [-B blacklist ...] [-t tags ...] [-o order] [query]\n\n", name);
-  fputs (
-    "-a    enable ascending order, desecending otherwise\n"
-    "-A    enable tag exclusive mode, otherwise union\n"
-    "-b    union of brandlist, otherwise empty\n"
-    "-B    exclusive blacklist of tags, otherwise empty\n"
-    "-t    list of tags, otherwise empty\n"
-    "-o    property to order results by (can be\n"
-    "      upload, views, likes, release or title),\n"
-    "      otherwise libido default\n"
-    "-n    number of results to show\n",
-    stderr);
-}
-
-static
-int search_req_from_args
-(
-  struct libido_search_request *req,
-  size_t *numres,
-  int argc,
-  char *const *argv
-)
-{
-  int opt;
-  int err = 0;
-  
-  req->ascending = false;
-  req->tag_and = true;
-  req->blacklist = NULL;
-  req->brands = NULL;
-  req->tags = NULL;
-  req->query = NULL;
-  req->order = LIBIDO_ORDER_BY_DEFAULT;
-
-  *numres = -1; // indefinite
-  
-  while ((opt = getopt (argc, argv, ":haAB:b:t:o:n:")) != -1)
-    switch (opt)
-    {
-      case 'h':
-        print_usage (argv [0]);
-        
-        ++err; // dirty hack
-        break;
-      case 'a': req->ascending = true;
-                break;
-      case 'A': req->tag_and = false;
-                break;
-      case 'B': split_string (&req->blacklist, optarg, ",");
-                break;
-      case 'b': split_string (&req->brands, optarg, ",");
-                break;
-      case 't': split_string (&req->tags, optarg, ",");
-                break;
-      case 'o': // long-live linear search!
-        for (size_t i = 0; i < 5; ++i)
-          if (strcmp (optarg, order_strs[i].repr) == 0)
-          {
-            req->order = order_strs[i].order;
-            ++err;
-            break;
-          }
-        
-        --err;
-        if (err)
-          fprintf (stderr, "invalid order %s\n", optarg);
-      break;
-      case 'n':
-        err += !sscanf(optarg, "%zu", numres);
-        break;
-      case ':':
-        fprintf (stderr, "option -%c requires an operand\n", opt);
-        --err;
-      break;
-      case '?':
-        fprintf (stderr, "unrecognized option -%c\n", opt);
-        --err;
-      break;
-    }
-  
-  argv += optind;
-  argc -= optind;
-  
-  if (argc > 0)
-    req->query = argv[0];
-  
-  return err;
-}
-
 int main (int argc, char **argv)
 {
   curl_global_init(CURL_GLOBAL_DEFAULT);
 
   struct libido_context *ido;
   enum libido_error idoerr;
-  int reti;
+  int reti, n_res;
   
   ido = libido_new ();
   if (!ido)
@@ -131,9 +25,9 @@ int main (int argc, char **argv)
     
   struct libido_search_request req;
   struct libido_search_response res;
-  size_t num_disp_res, res_idx;
+  size_t n_res_max;
   
-  reti = search_req_from_args (&req, &num_disp_res, argc, argv);
+  reti = search_req_from_args (&req, &n_res_max, argc, argv);
   if (reti != 0)
   {
     if (reti < 0)
@@ -144,9 +38,6 @@ int main (int argc, char **argv)
     goto drop;
   }
   
-  req.page_no = 0;
-  res_idx = 0;
-
   req.page_no = 0;
   n_res = 0;
 
